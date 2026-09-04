aeea34f42de39c9f36a3d6642568bb99ed75be61
linyabin <linyabin@joyintech.com>
2026-09-04 13:23:55 +0800

    fix: update deployment branch from main to master in workflow and set base path in VuePress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  base: '/vuepress-starter/',
   bundler: viteBundler(),
   theme: defaultTheme(),
 })
